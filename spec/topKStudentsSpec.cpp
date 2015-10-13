8cbd6578338f16a53db619a733bcd3d82893fd48
hemanth.chinthalapudi <hemanth.chinthalapudi@kony.com>
2015-10-14 01:02:46 +0530

    Corrected top k students program logic.

--- spec/topKStudentsSpec.cpp
+++ spec/topKStudentsSpec.cpp
@@ -10,6 +10,36 @@ namespace spec
 	{
 	public:
 
+		void swap(int *a, int *b) {
+			*a = *a + *b;
+			*b = *a - *b;
+			*a = *a - *b;
+		}
+
+		void sort(int arr[], int len) {
+			for (int i = 0; i < len - 1; ++i) {
+				for (int j = 0; j < len - i - 1; ++j) {
+					if (arr[j] > arr[j + 1]) {
+						swap(&arr[j], &arr[j + 1]);
+					}
+				}
+			}
+		}
+
+		bool validate(int expected[], int len, struct student **result) {
+			int *actual = (int *)malloc(len * sizeof(int));
+			for (int i = 0; i < len; ++i) {
+				actual[i] = result[i]->score;
+			}
+			sort(actual, len);
+			for (int i = 0; i < len; ++i) {
+				if (expected[i] != actual[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+
 		TEST_METHOD(negativeKValue)
 		{
 			struct student students[3] = { { "stud1", 70 }, { "stud2", 60 }, { "stud3", 50 } };
@@ -34,31 +64,30 @@ namespace spec
 
 		TEST_METHOD(KGreaterThanLength)
 		{
-			struct student students[3] = { { "stud1", 50 }, { "stud2", 60 }, { "stud3", 70 } };
+			struct student students[3] = { { "stud1", 60 }, { "stud2", 70 }, { "stud3", 50 } };
 			struct student **result = topKStudents(students, 3, 5);
-			Assert::AreEqual(50, result[0]->score, L"K greater than length case failed.", LINE_INFO());
-			Assert::AreEqual(60, result[1]->score, L"K greater than length case failed.", LINE_INFO());
-			Assert::AreEqual(70, result[2]->score, L"K greater than length case failed.", LINE_INFO());
+			int exp[] = { 50, 60, 70 };
+			Assert::IsTrue(validate(exp, 3, result), L"K greater than length case failed.", LINE_INFO());
 		}
 
 		TEST_METHOD(KValue1)
 		{
 			struct student students[3] = { { "stud1", 50 }, { "stud2", 60 }, { "stud3", 70 } };
 			struct student **result = topKStudents(students, 3, 1);
-			Assert::AreEqual(50, result[0]->score, L"K value 1 case failed.", LINE_INFO());
+			Assert::AreEqual(70, result[0]->score, L"K value 1 case failed.", LINE_INFO());
 		}
 
 		TEST_METHOD(positiveKValue)
 		{
 			struct student students[3] = { { "stud1", 50 }, { "stud2", 60 }, { "stud3", 70 } };
 			struct student **result = topKStudents(students, 3, 2);
-			Assert::AreEqual(50, result[0]->score, L"K value 2 case failed.", LINE_INFO());
-			Assert::AreEqual(60, result[1]->score, L"K value 2 case failed.", LINE_INFO());
-			struct student students1[3] = { { "stud1", 50 }, { "stud2", 60 }, { "stud3", 70 } };
-			struct student **result1 = topKStudents(students1, 3, 3);
-			Assert::AreEqual(50, result1[0]->score, L"K value 3 case failed.", LINE_INFO());
-			Assert::AreEqual(60, result1[1]->score, L"K value 3 case failed.", LINE_INFO());
-			Assert::AreEqual(70, result1[2]->score, L"K value 3 case failed.", LINE_INFO());
+			int exp[] = { 60, 70 };
+			Assert::IsTrue(validate(exp, 2, result), L"K value 2 case failed.", LINE_INFO());
+
+			struct student students1[6] = { { "stud1", 30 }, { "stud2", 10 }, { "stud3", 60 }, { "stud4", 50 }, { "stud5", 20 }, { "stud6", 40 } };
+			struct student **result1 = topKStudents(students1, 6, 3);
+			int exp1[] = { 40, 50, 60 };
+			Assert::IsTrue(validate(exp1, 3, result1), L"K value 3 case failed.", LINE_INFO());
 		}
 
 	};
--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -2,11 +2,11 @@
 OVERVIEW:  You are given scores for students of a class in alphabetical order. Write a function that returns the names and scores of top K students.
 Input is array of structures of type student (each student has name and score).
 E.g.: If input is [ { "stud1", 20 }, { "stud2", 30 }, { "stud3", 40 } ] (three students with marks 20, 30 , 40) and K value is 2.
-return top 2 students [ { "stud1", 20 }, { "stud2", 30 } ]
+return top 2 students [ { "stud2", 30 }, { "stud3", 40 } ]
 
 INPUTS: array of structures of type student, length of the array, integer K.
 
-OUTPUT: Return top K students from array.
+OUTPUT: Return top K students from array. Output need not be in decreasing order of scores.
 
 ERROR CASES: Return NULL for invalid inputs.
 
